4cc55e9a8f71bfab62bc01deb6b332ad41920912
Denise Machado <demachado@deloitte.pt>
2020-11-11 12:20:17 +0000

    Started specs

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,10 +7,17 @@ class Card
   end
 end
 
-RSpec.describe 'Card' do
-  it 'has a type' do
-    card = Card.new('Ace of Spades')
+RSpec.describe Card do
+  it 'has a rank' do
+    card = Card.new('Ace', 'Spades')
 
-    expect(card.type).to eq('Ace of Spades')
+    expect(card.rank).to eq('Ace')
   end
+
+  it 'has a suit' do
+    card = Card.new('Ace', 'Spades')
+
+    expect(card.rank).to eq('Spades')
+  end
+
 end
